58fb2588cd92d9739df28db7d98e40c307bdf814
Maz Ahmadi <hello@maziar.io>
2022-01-02 14:42:56 -0800

    v1.2.2 - Fix headers in React Native

--- BaseAPIClient.js
+++ BaseAPIClient.js
@@ -24,7 +24,7 @@ export default class BaseAPIClient {
   }) {
     this.host = host;
     this.contentType = contentType;
-    this.headers = new Headers(headers);
+    this.headers = headers;
     this.payloadSignMethod = payloadSignMethod;
     this.fetch = fetch;
   }
