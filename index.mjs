03c27d1198eb7c182b83eb42576141d85e132118
Maz Ahmadi <hello@maziar.io>
2022-01-19 00:15:07 -0800

    v1.3.4 - Refactor RN support

--- index.mjs
+++ index.mjs
@@ -1,2 +1,7 @@
-import APIClient from './APIClient.mjs';
+import getPlatform, { PLATFORM } from './utils/getPlatform.mjs';
+
+const { default: APIClient } = await import(
+  getPlatform() === PLATFORM.REACT_NATIVE ? './APIClient.native.mjs' : './APIClient.mjs'
+);
+
 export default APIClient;
