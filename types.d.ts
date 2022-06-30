65e4b45085b8433a0395bd30917013b7ded403c5
Maz Ahmadi <hello@maziar.io>
2022-06-29 18:39:58 -0700

    v2.0.6 - Update type declerations

--- types.d.ts
+++ types.d.ts
@@ -1,45 +1,43 @@
 import type { RequestInitWithRetry } from 'fetch-retry';
 
-declare module '@cmdshepard/api-client' {
-  enum CONTENT_TYPE {
-    JSON = 'application/json',
-    FORM_URL_ENCODED = 'application/x-www-form-urlencoded',
-  }
+declare enum CONTENT_TYPE {
+  JSON = 'application/json',
+  FORM_URL_ENCODED = 'application/x-www-form-urlencoded',
+}
 
-  type headers = {
-    [key: string]: string
+declare type headers = {
+  [key: string]: string
+};
+
+export class APIClient {
+  public static CONTENT_TYPE: {
+    JSON: 'application/json',
+    FORM_URL_ENCODED: 'application/x-www-form-urlencoded',
   };
 
-  export class APIClient {
-    public static CONTENT_TYPE: {
-      JSON: 'application/json',
-      FORM_URL_ENCODED: 'application/x-www-form-urlencoded',
-    };
+  constructor(initOpts: {
+    host?: string;
+    contentType?: CONTENT_TYPE;
+    headers?: headers;
+    payloadSignMethod?: (body: any) => any;
+    retryOpts: RequestInitWithRetry;
+  });
 
-    constructor(initOpts: {
-      host?: string;
-      contentType?: CONTENT_TYPE;
-      headers?: headers;
-      payloadSignMethod?: (body: any) => any;
-      retryOpts: RequestInitWithRetry;
-    });
+  public get(path: string, headers?: headers);
 
-    public get(path: string, headers?: headers);
+  public post(path: string, body?: any, headers?: headers);
 
-    public post(path: string, body?: any, headers?: headers);
+  public patch(path: string, body?: any, headers?: headers);
 
-    public patch(path: string, body?: any, headers?: headers);
+  public put(path: string, body?: any, headers?: headers);
 
-    public put(path: string, body?: any, headers?: headers);
-
-    public del(path: string, body?: any, headers?: headers);
-  }
-
-  export class APIResponseError extends Error {
-    public status: number;
-    public statusText: string;
-    public body: any;
-  }
-
-  export class NetworkError extends Error {}
+  public del(path: string, body?: any, headers?: headers);
 }
+
+export class APIResponseError extends Error {
+  public status: number;
+  public statusText: string;
+  public body: any;
+}
+
+export class NetworkError extends Error {}
